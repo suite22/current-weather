26b5832c92035c9d0fc3b467aede4e13e581b9d9
Ben Goertz <suite22@users.noreply.github.com>
2016-10-17 19:51:51 -0700

    Initial project creation.

---
+++ CurrentWeather/CurrentWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CurrentWeather
+//
+//  Created by Ben Goertz on 10/17/16.
+//  Copyright © 2016 Ben Goertz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

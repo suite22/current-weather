c02e76f807a4c3c8d67f2244ddec51641bccec77
Ben Goertz <suite22@users.noreply.github.com>
2016-10-19 09:16:22 -0700

    Getting location through CoreLocation. Still need to hook it up to the weather fetch.

--- CurrentWeather/CurrentWeather/ViewController.swift
+++ CurrentWeather/CurrentWeather/ViewController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var currentTempLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
+    // cLat and cLon should be set at the same time, so only using the didSet on cLat
+    internal var cLat: CLLocationDegrees? {
+        didSet {
+            refreshWeather()
+        }
+    }
+    internal var cLon: CLLocationDegrees?
     
     private var currentWeather: WeatherModel? {
         didSet {
@@ -21,7 +32,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        refreshWeather()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.distanceFilter = 3000
+        
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            print("Authorized to use location")
+        case .notDetermined:
+            self.locationManager.requestWhenInUseAuthorization()
+        default:
+            print("Unable to use location services")
+        }
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +72,24 @@ class ViewController: UIViewController {
         
         let tempRounded = round(weather.temperature)
         currentTempLabel.text = "\(tempRounded)°"
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location Manager failed Captain!: ", error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // since they're returned in chronological order, we really just want the last one
+        guard let location = locations.last else {
+            print("Unable to get the most recent location from the locations array.")
+            return
+        }
+        
+        cLat = location.coordinate.latitude
+        cLon = location.coordinate.longitude
     }
 }
 

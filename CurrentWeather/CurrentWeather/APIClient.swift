0cf379d7e459763ed683edcc356a3b22d22d1944
Ben Goertz <suite22@users.noreply.github.com>
2016-10-19 19:19:47 -0700

    Cleaning up APIClient errors.

--- CurrentWeather/CurrentWeather/APIClient.swift
+++ CurrentWeather/CurrentWeather/APIClient.swift
@@ -9,7 +9,9 @@
 import Foundation
 
 enum APIError: Error {
-    case createRequest(String)
+    case createRequestFailed(String)
+    case emptyDataResponse(String)
+    case jsonDeserializeFailed(String)
 }
 
 class APIClient {
@@ -18,29 +20,34 @@ class APIClient {
     
     func fetchWeather(request: APIRequest, completion: @escaping (WeatherModel?, Error?) -> Void) {
         guard let weatherRequest = request.weatherRequest() else {
-            completion(nil, APIError.createRequest("Failed to create the weather request."))
+            completion(nil, APIError.createRequestFailed("Failed to create the weather request."))
             return
         }
         
         let task = session.dataTask(with: weatherRequest, completionHandler: { (data, response, error) -> Void in
             
             guard error == nil else {
-                print("Got error response:", error)
                 completion(nil, error)
                 return
             }
             
-            if let data = data,
-                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                print("Printing JSON:", json)
-                if let json = json {
-                    if let weather = WeatherModel(json: json) {
-                        completion(weather, nil)
-                    }
+            guard let data = data else {
+                completion(nil, APIError.emptyDataResponse("Data response nil."))
+                return
+            }
+            
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                completion(nil, APIError.jsonDeserializeFailed("JSON deserialization failed."))
+                return
+            }
+            
+            print("Printing JSON:", json)
+            if let json = json {
+                if let weather = WeatherModel(json: json) {
+                    completion(weather, nil)
                 }
             }
         })
-        
         task.resume()
     }
 }
